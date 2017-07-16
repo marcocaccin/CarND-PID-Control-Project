0b29ace2fefef2d62e98c0481d694ad513de439c
Marco Caccin <marcocaccin@users.noreply.github.com>
2017-07-16 19:08:23 +0200

    Update PID.h

--- src/PID.h
+++ src/PID.h
@@ -1,21 +1,18 @@
 #ifndef PID_H
 #define PID_H
 
+#include <vector>
+
+
 class PID {
 public:
-  /*
-  * Errors
-  */
+  // Errors
   double p_error;
   double i_error;
   double d_error;
+  // Coefficients
+  std::vector<double> Kpid;
 
-  /*
-  * Coefficients
-  */ 
-  double Kp;
-  double Ki;
-  double Kd;
 
   /*
   * Constructor
@@ -41,6 +38,42 @@ public:
   * Calculate the total PID error.
   */
   double TotalError();
+};
+
+
+
+class Twiddle {
+public:
+  /*
+  * Coefficients
+  */
+  std::vector<double> dp_;
+  // Twiddle params
+  int cnt;
+  int idx;
+  int attempt;
+  double best_err;
+  double cur_err;
+
+  /*
+  * Constructor
+  */
+  Twiddle();
+
+  /*
+  * Destructor.
+  */
+  virtual ~Twiddle();
+
+  /*
+  * Initialize Twiddle.
+  */
+  void Init(double dKp, double dKi, double dKd);
+
+  /*
+  * Update the twiddle state given the integrated cross track error.
+  */
+  void NewParams(double& i_error, std::vector<double>& Kpid);
 };
 
 #endif /* PID_H */
